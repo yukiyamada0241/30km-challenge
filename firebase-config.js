0ba32ce0da159b6725b74c26102dd45147d056fd
yukiyamada0241 <yukiyamada0241@gmail.com>
2026-09-14 11:26:15 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCB0G2I8SrJd4SoI0pOgN2DChxKZ8jhMjK",
+  apiKey: "AIzaSyCBOG2I8SrJd4SoIOpOgN2DChxKZ8jhMjk",
   authDomain: "km-challenge-e5c87.firebaseapp.com",
   databaseURL: "https://km-challenge-e5c87-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "km-challenge-e5c87",
   storageBucket: "km-challenge-e5c87.firebasestorage.app",
   messagingSenderId: "695231456027",
-  appId: "1:695231456027:web:26d0f83e0b3cc17ee3a9be"
+  appId: "1:695231456027:web:f15d9f47f244a5c1e3a9be",
+  measurementId: "G-BEDC7SF1WQ"
 };
