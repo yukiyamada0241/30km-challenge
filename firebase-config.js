c7729f305f7166f4a6c022cd0810d176bd773e9e
yukiyamada0241 <yukiyamada0241@gmail.com>
2026-09-14 11:16:14 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCB0G2I8SrJd4SoI0pOgN2DChxKZ8jhMjk",
+  apiKey: "AIzaSyCB0G2I8SrJd4SoI0pOgN2DChxKZ8jhMjK",
   authDomain: "km-challenge-e5c87.firebaseapp.com",
   databaseURL: "https://km-challenge-e5c87-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "km-challenge-e5c87",
