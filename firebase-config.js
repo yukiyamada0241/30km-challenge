5cfea81dab7b1b193a06a46345f6247365e35a6c
yukiyamada0241 <yukiyamada0241@gmail.com>
2026-09-14 10:55:28 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCB0G2I8SrJd4SoI0pOgN2DChxKZ8jhMjk",
+  authDomain: "km-challenge-e5c87.firebaseapp.com",
+  databaseURL: "https://km-challenge-e5c87-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "km-challenge-e5c87",
+  storageBucket: "km-challenge-e5c87.firebasestorage.app",
+  messagingSenderId: "695231456027",
+  appId: "1:695231456027:web:26d0f83e0b3cc17ee3a9be"
+};
